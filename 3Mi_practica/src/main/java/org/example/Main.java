b8c0dd83eb6d66587cf7acf9c965bc8eddeef4a6
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-09 20:52:32 -0400

    3Mi_practica: metodo high-order implementado

--- 3Mi_practica/src/main/java/org/example/Main.java
+++ 3Mi_practica/src/main/java/org/example/Main.java
@@ -28,6 +28,24 @@ public class Main {
             return "Hola soy Tux y recibi el siguiente parametro: " + param + " por interfaz funcional";
         };
         System.out.println(tux1.doSomething("VALOR FUNCIONAL"));
+
+        MyFunctionalInterface tux2 = (x) -> "Hola soy Tux2 y recibi el siguiente parametro: " + x + " por interfaz funcional";
+        System.out.println(tux2.doSomething("VALOR FUNCIONAL"));
+
+        // hacemos uso de la funcion high-order
+        doSomethingHighOrder(tux1);
+        doSomethingHighOrder(tux2);
+        doSomethingHighOrder(x -> "Hola soy Metodo Anonimo y recibi el siguiente parametro: " + x + " por interfaz funcional");
+
+        var tux = new Tux();
+        doSomethingHighOrder(tux::doSomethingRespetandoFirma);
+
     }
+
+        //El argumento valida la firma del metodo funcional
+        public static void doSomethingHighOrder(MyFunctionalInterface comportamiento){
+            // Ejecuto el comportamiento que recibo como argumento
+            System.out.println(comportamiento.doSomething("VALOR HIGH-ORDER"));
+        }
 
 }
--- 3Mi_practica/src/main/java/org/example/Tux.java
+++ 3Mi_practica/src/main/java/org/example/Tux.java
@@ -3,6 +3,11 @@ package org.example;
 public class Tux implements MyFunctionalInterface {
     @Override
     public String doSomething(String param) {
-        return "Hola soy Tux y recibi el sieguiente paremetro: " + param;
+        return "Hola soy Tux y recibi el siguiente paremetro: " + param;
     }
+
+    public String doSomethingRespetandoFirma(String param) {
+        return "Hola soy Tux y recibi el siguiente paremetro respentando la firma a codigo ya existente: " + param;
+    }
+
 }
